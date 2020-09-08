9efb9c342580e0f6e191d5a4dc5db34b2ee17d5b
Keisuke69 <providence000@gmail.com>
2020-09-08 14:45:13 +0000

    Modify getting title and contents from wordpress.com

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,24 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        serverless-newworld-20200909
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <ul>
+      <li v-for="(post, index) in posts.posts" :key="index">
+          <a v-bind:href="'/post/' + post.ID">{{post.title}}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $axios }) {
+    const url = "https://public-api.wordpress.com/rest/v1.1/sites/keisuke69.wordpress.com/posts/" ;
+    const response = await $axios.$get(url);
+    console.log(response);
+    return {
+      posts: response,
+    };
+  },
+};
 </script>
 
 <style>
@@ -44,35 +34,5 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
---
+++ pages/post/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-html="post.content" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios,params}) {
+  console.log(params.id);
+    const url =
+      "https://public-api.wordpress.com/rest/v1.1/sites/keisuke69.wordpress.com/posts/"+ params.id;
+    const response = await $axios.$get(url);
+
+    return {
+      post: response,
+    };
+  },
+};
+</script>
