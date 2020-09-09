6c95c1c85a13bef6b13ed9aa352f3344bba485d0
Keisuke69 <providence000@gmail.com>
2020-09-09 05:06:55 +0000

    Change to universal mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,12 +4,12 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
-  mode: 'spa',
+  mode: 'universal',
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'static',
+  target: 'server',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
